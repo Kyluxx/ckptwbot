4f3fabf2aad912c4c415abf9ee4072657098df42
Kyluxx <rizkypratamaikok@gmail.com>
2025-04-24 23:03:18 +0700

    +

--- tools/general.js
+++ tools/general.js
@@ -1,6 +1,34 @@
 // Impor modul dan dependensi yang diperlukan
 const api = require("./api.js");
-const uploader = require("@zanixongroup/uploader");
+const uploader = async () => {
+  const fetch = require('node-fetch');
+const FormData = require('form-data');
+const { fromBuffer } = require('file-type');
+/**
+* Upload image to url
+* Supported mimetype:
+* - `image/jpeg`
+* - `image/jpg`
+* - `image/png`
+* - `video/mp4`
+* - `all files`
+* @param {Buffer} buffer Image Buffer
+*/
+
+module.exports = async (buffer) => {
+  let { ext } = await fromBuffer(buffer);
+  const bodyForm = new FormData();
+  bodyForm.append("file", buffer, "file." + ext);
+  
+  const response = await fetch("https://file.idnet.my.id/api/upload.php", {
+    method: "POST",
+    body: bodyForm,
+  });
+
+  const result = await response.json();
+  return result.file.url;
+}
+}
 const axios = require("axios");
 const didYouMean = require("didyoumean");
 const util = require("node:util");
